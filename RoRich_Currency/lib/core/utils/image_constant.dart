69764517aa78f2e870d62ec19c90753aab9c1d45
prae <121804853+prwsphng@users.noreply.github.com>
2024-02-07 20:40:41 +0700

    Add files via upload

---
+++ RoRich_Currency/lib/core/utils/image_constant.dart
@@ -0,0 +1,17 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // convert page images
+  static String imgFloatingIcon = '$imagePath/img_floating_icon.svg';
+
+  static String imgTelevision = '$imagePath/img_television.svg';
+
+  static String imgRoro42 = '$imagePath/img_roro_4_2.png';
+
+  static String imgInfographic31 = '$imagePath/img_infographic_3_1.png';
+
+  static String imgArrow3 = '$imagePath/img_arrow_3.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
